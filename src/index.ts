312ef50e7053e154bf4e9b62f0c99c40a4349099
Teakowa <27560638+Teakowa@users.noreply.github.com>
2023-11-23 16:02:07 +0800

    style: fix typo
    Signed-off-by: Teakowa <27560638+Teakowa@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { Hono } from 'hono'
-import { poweredBy } from 'hono/powered-by'
+import { Hono } from "hono";
+import { poweredBy } from "hono/powered-by";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', poweredBy())
+app.use("*", poweredBy());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
 // Custom Not Found Message
 app.notFound((c) => {
-  return c.text('Custom 404 Not Found', 404)
-})
+  return c.text("404 Not Found", 404);
+});
 
 // Error handling
 app.onError((err, c) => {
-  console.error(`${err}`)
-  return c.text('Custom Error Message', 500)
-})
+  console.error(`${err}`);
+  return c.text("Custom Error Message", 500);
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+export default app;
