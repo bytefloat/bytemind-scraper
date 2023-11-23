c3f15e5ba2815042db1ea1a3d93cc50cc600c0a8
Teakowa <27560638+Teakowa@users.noreply.github.com>
2023-11-23 15:38:21 +0800

    🎉 Initial Commit
    Signed-off-by: Teakowa <27560638+Teakowa@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ const app = new Hono()
 
 app.use('*', poweredBy())
 
+// Custom Not Found Message
+app.notFound((c) => {
+  return c.text('Custom 404 Not Found', 404)
+})
+
+// Error handling
+app.onError((err, c) => {
+  console.error(`${err}`)
+  return c.text('Custom Error Message', 500)
+})
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
