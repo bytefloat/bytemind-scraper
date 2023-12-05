aa80d0ef910e3208927bada14f494adc2a551d36
Teakowa <27560638+Teakowa@users.noreply.github.com>
2023-12-05 20:13:33 +0800

    feat: add gallerix
    Signed-off-by: Teakowa <27560638+Teakowa@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -80,6 +80,68 @@ app.get("/nrta", async (c) => {
   return c.json(records);
 });
 
+app.get("/gallerix", async (c) => {
+  const url = "https://gallerix.asia/a1/";
+
+  const headers = {
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "accept-language": "en-US,en;q=0.8",
+    "upgrade-insecure-requests": "1",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+    "user-agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 uacq",
+  };
+
+  const response = await fetch(url, {
+    headers: headers,
+    cf: {
+      cacheTtlByStatus: { "200-299": 86400, "4xx": 1, "5xx": 0 },
+    },
+  });
+
+  // 确保请求成功
+  if (!response.ok) {
+    return new Response("Failed to fetch the Data", {
+      status: response.status,
+    });
+  }
+
+  const htmlText = await response.text();
+
+  const $ = cheerio.load(htmlText);
+
+  const records = [];
+
+  $(".pic").each((index, element) => {
+    const $record = $(element);
+
+    const imageSrc = $record.find("img").attr("src");
+    const imageUrl = imageSrc.startsWith("//") ? "https:" + imageSrc : imageSrc;
+
+    // Get the artist's name and the number of images
+    // Note that the artist's name is in the 'alt' attribute of the image, and the number is bolded just before the <br> tag
+    const artistName = $record.find("img").attr("alt");
+    const numberOfImages = parseInt(
+      $record
+        .find("b")
+        .first()
+        .text()
+        .replace(/[^0-9]/g, ""),
+    ); // Remove all non-digit characters
+
+    const record = {
+      imageUrl,
+      artistName,
+      numberOfImages,
+    };
+
+    records.push(record);
+  });
+
+  return c.json(records);
+});
+
 // Custom Not Found Message
 app.notFound((c) => {
   return c.text("404 Not Found", 404);
