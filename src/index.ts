006f5618351c4e00312195f84bf992a398c6a939
Teakowa <27560638+Teakowa@users.noreply.github.com>
2023-12-05 16:34:40 +0800

    build: add logger
    Signed-off-by: Teakowa <27560638+Teakowa@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { poweredBy } from "hono/powered-by";
 import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
 app.use("*", poweredBy());
 app.use("*", cors());
+app.use("*", logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
