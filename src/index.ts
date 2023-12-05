1370e334c02dc1458b8f2b1428bf5249b7940885
Teakowa <27560638+Teakowa@users.noreply.github.com>
2023-12-05 16:36:06 +0800

    feat(nrta): 添加 nrta 实现
    Signed-off-by: Teakowa <27560638+Teakowa@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { poweredBy } from "hono/powered-by";
 import { cors } from "hono/cors";
+import * as cheerio from "cheerio";
 import { logger } from "hono/logger";
 
 const app = new Hono();
@@ -17,6 +18,68 @@ app.get("/ping", (c) => {
   return c.text("pong");
 });
 
+app.get("/nrta", async (c) => {
+  const url = "https://www.nrta.gov.cn/col/col38/index.html";
+
+  const headers = {
+    "accept-language": "en-US,en;q=0.6",
+    "Referer": "https://www.nrta.gov.cn/col/col38/index.html",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+    "user-agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 uacq",
+  };
+
+  const body =
+    "col=1&webid=1&path=http%3A%2F%2Fwww.nrta.gov.cn%2F&columnid=38&sourceContentType=1&unitid=14416&webname=%E5%9B%BD%E5%AE%B6%E5%B9%BF%E6%92%AD%E7%94%B5%E8%A7%86%E6%80%BB%E5%B1%80&permissiontype=0%0A";
+
+  const response = await fetch(url, {
+    headers: headers,
+    cf: {
+      cacheTtlByStatus: { "200-299": 86400, "4xx": 1, "5xx": 0 },
+    },
+  });
+
+  // 确保请求成功
+  if (!response.ok) {
+    return new Response("Failed to fetch the Data", {
+      status: response.status,
+    });
+  }
+
+  // 获取HTML文本
+  const htmlText = await response.text();
+
+  const $ = cheerio.load(htmlText, {
+    xmlMode: true,
+  });
+
+  const records = [];
+
+  $("recordset record").each((index, element) => {
+    const $record = $(element);
+
+    // Extract link, title, and date from CDATA
+    const cdataContent = $record.text();
+
+    // 从每个记录中提取数据
+    const link = $(cdataContent).find("a").attr("href");
+    const date = $(cdataContent).find("span").text();
+    const title = $(cdataContent).find("a").text().replace(date, "").trim();
+
+    const record = {
+      title,
+      link,
+      date,
+    };
+
+    if (title.includes("发行许可")) {
+      records.push(record);
+    }
+  });
+
+  return c.json(records);
+});
+
 // Custom Not Found Message
 app.notFound((c) => {
   return c.text("404 Not Found", 404);
@@ -24,7 +87,7 @@ app.notFound((c) => {
 
 // Error handling
 app.onError((err, c) => {
-  console.error(`${err}`);
+  console.log(`${err}`);
   return c.text("Custom Error Message", 500);
 });
 
